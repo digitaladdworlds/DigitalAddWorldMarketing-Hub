d5760421c5a240bb1dffb1d5215f054c8f7b00d0
digitaladdworlds <digitaladdworld799@gmail.com>
2026-08-01 01:53:59 -0700

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5002',
+        target: 'https://digitaladdworldmarketing-hub-backend.onrender.com/api',
         changeOrigin: true
       }
     }
